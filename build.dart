398167af6e55b475bc3bf575adb989c5a02538c9
Sigmund Cherem <sigmund@google.com>
2013-04-23 10:35:35 -0700

    Add support for 'H' (toggle highlight), play with using bindings in a slide
    Review URL: https://chromiumcodereview.appspot.com//14317012

--- build.dart
+++ build.dart
@@ -2,5 +2,7 @@ import 'dart:io';
 import 'package:web_ui/component_build.dart';
 
 main() {
-  build(new Options().arguments, ['web/slidedeck.html']);
+  var args = new Options().arguments.toList();
+  args.addAll(['--', '--basedir', '.']);
+  build(args, ['web/slidedeck.html']);
 }
